5cb38f627614fd908ab9c4330a7545210c57ff07
Cole Dorman <coledorman12@gmail.com>
2025-08-28 08:36:45 -0500

    KittyKatta done

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/>  r click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    for (int i = 1; i <= 100; i++) {
+        std::string formatted_text = "";
+        if (i % 3 == 0) {
+            formatted_text = "Kitty";
+        }
+        if (i % 5 == 0) {
+            formatted_text = formatted_text + "Katta";
+        }
+        if (formatted_text == "") {
+            formatted_text = std::to_string(i);
+        }
+        std::cout << formatted_text << std::endl;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
