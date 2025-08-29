e90487b92a4f7f7a4023d0d31d40fa26c7e0e4b5
Cole Dorman <coledorman12@gmail.com>
2025-08-29 13:25:57 -0500

    Major progress, still need to finalize logic

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+// Functions
+float FahrenheitToCelsius(float fahrenheit) {
+    return (fahrenheit - 32.0f) * 5.0f / 9.0f;
+}
+float CelsiusToFahrenheit(float celsius)
+{
+    return celsius * (9.0f / 5.0f) + 32.0f;
+}
+// Main
 int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
+    //KittyKatta
     for (int i = 1; i <= 100; i++) {
         std::string formatted_text = "";
         if (i % 3 == 0) {
@@ -15,6 +22,34 @@ int main() {
             formatted_text = std::to_string(i);
         }
         std::cout << formatted_text << std::endl;
+    }
+
+    //Chirp Chirp Chirp
+    std::string inputTemperature;
+    std::cout << "Input temperature as a format of NumberTemperatureType, Ex. 14f";
+    bool isInputValid = false;
+    while (!isInputValid) {
+        std::cin >> inputTemperature;
+        isInputValid = true;
+        char lastCharOfInput = inputTemperature[inputTemperature.length()-1];
+        lastCharOfInput = tolower(lastCharOfInput);
+        if (lastCharOfInput == 'c' || lastCharOfInput == 'f') {
+            for (int i = 0; i < inputTemperature.length() - 1; i++) {
+                if (!std::isdigit(inputTemperature[i])) {
+                    std::cout << "Invalid input, Ex. 14f";
+                    isInputValid = false;
+                    break;
+                }
+            }
+        }
+        else {
+            isInputValid = false;
+            std::cout << "Missing unit, Ex. 14f";
+            break;
+        }
+    }
+    if (isInputValid) {
+        std::cout << inputTemperature.substr(0, inputTemperature.length()-1);
     }
 
     return 0;
