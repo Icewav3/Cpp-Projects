2f9f893091f344b604a597cac97a0181c129b536
Cole Dorman <coledorman12@gmail.com>
2025-09-03 23:14:36 -0500

    idrk what im doing tbh

---
+++ Assignment1/main.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef FIRST_TEST_H
+#define FIRST_TEST_H
+
+#include <iostream>
+#include <string>
+
+// Temperature conversion functions
+float FahrenheitToCelsius(float fahrenheit);
+float CelsiusToFahrenheit(float celsius);
+float ChirpsToFahrenheit(float chirps);
+bool IsValidTemperature(float fahrenheit);
+
+// Password validation functions
+bool hasLowerCase(const std::string& password);
+bool hasUpperCase(const std::string& password);
+bool hasNumber(const std::string& password);
+std::string getPassword();
+
+// Random number generation
+int randomNumber(const int min, const int max);
+
+#endif // FIRST_TEST_H
