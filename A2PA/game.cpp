92b0f5e45a63b47d3ef21f8f47aa37a124a2ac56
Cole Dorman <cdorman@academic.rrc.ca>
2025-09-10 14:51:54 -0500

    Raylib Setup attempt

--- A2PA/game.cpp
+++ A2PA/game.cpp
@@ -1,2 +1,27 @@
 #include "game.hpp"
-#include "raylib"
+#include <raylib.h>
+
+int main() {
+    // Initialize raylib window
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib Game Window");
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Update game logic here
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    // Clean up
+    CloseWindow();
+
+    return 0;
+}
