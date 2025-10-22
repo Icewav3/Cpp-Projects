f8fddb9fe47bec04e33f41d16ee3148931268a59
Cole Dorman <cdorman@academic.rrc.ca>
2025-10-22 15:10:58 -0500

    added demo empty test proj
    also removed the unused resources folder from cmakelists

--- ReactorSim/main.cpp
+++ ReactorSim/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+// Add raylib header
+#include "raylib.h"
+
 int main() {
-	return 0;
+    // Initialize the window
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib empty window example");
+
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello, world!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();  // Close the window and OpenGL context
+
+    return 0;
 }
