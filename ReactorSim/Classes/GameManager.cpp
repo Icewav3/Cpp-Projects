9492c50c93a4863d73fa8c6e0804f54d6f8034c4
Cole Dorman <coledorman12@gmail.com>
2025-10-22 13:25:56 -0500

    Created a GameManager class

---
+++ ReactorSim/Classes/GameManager.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by coled on 10/21/2025.
+//
+
+#include "GameManager.h"
---
+++ ReactorSim/Classes/GameManager.h
@@ -0,0 +1,13 @@
+﻿//
+// Created by coled on 10/21/2025.
+//
+
+#ifndef CPP_PROJECTS_GAMEMANAGER_H
+#define CPP_PROJECTS_GAMEMANAGER_H
+
+
+class GameManager {
+};
+
+
+#endif //CPP_PROJECTS_GAMEMANAGER_H
